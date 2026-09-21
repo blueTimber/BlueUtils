81b1fae0143b2a502fc6f4b4c06482d83e81cea0
Jonte <53584088+blueTimber@users.noreply.github.com>
2026-09-22 00:59:52 +0200

    Add A*, a priority queue and a binary heap.
    - The A* implementation has the standard time complexity, but is optimised for memory complexity, especially for multiple runs.
    - The priority queue has a doubly linked list implementation, which mean add has O(n) time complexity, but pop has o(1). It does have some overhead for the wrapper classes, but it allows for iteration and more manipulation in the future.
    - The binary heap is a standard binary heap.

--- Runtime/Geometry/Intersection/IntersectionClosest2D.cs
+++ Runtime/Geometry/Intersection/IntersectionClosest2D.cs
@@ -8,7 +8,7 @@ namespace BlueUtils.Geometry
 		/// Calculate the closest point on a line to a given point in 2D space.
 		/// The point is clamped to the line segment.
 		/// </summary>
-		public static Vector2 ClosestPointOnLine(Vector2 lineStart, Vector2 lineEnd, Vector2 point)
+		public static Vector2 ClosestPointOnLineSegment(Vector2 lineStart, Vector2 lineEnd, Vector2 point)
 		{
 			Vector2 lineDir = (lineEnd - lineStart).normalized;
 			float t = Vector2.Dot(point - lineStart, lineDir);
@@ -30,7 +30,7 @@ namespace BlueUtils.Geometry
 		/// <returns>Returns the closest points on the two lines as a tuple of Vector2. 
 		/// If the lines are parallel, returns null.
 		/// Item1 is on line1 and Item2 is on line2.</returns>
-		public static (Vector2, Vector2)? ClosestPointBetweenLines(Vector2 line1start, Vector2 line1end, Vector2 line2start, Vector2 line2end)
+		public static (Vector2, Vector2)? ClosestPointBetweenLineSegments(Vector2 line1start, Vector2 line1end, Vector2 line2start, Vector2 line2end)
 		{
 			// Calculate the direction vectors of the lines
 			Vector2 line1Dir = (line1end - line1start).normalized;
@@ -50,7 +50,7 @@ namespace BlueUtils.Geometry
 			(Vector2, Vector2) closestPoints = (Vector2.zero, Vector2.zero);
 
 			// Line 2 start
-			(Vector2, Vector2) candidate = (ClosestPointOnLine(line1start, line1end, line2start), line2start);
+			(Vector2, Vector2) candidate = (ClosestPointOnLineSegment(line1start, line1end, line2start), line2start);
 			if ((candidate.Item2 - candidate.Item1).sqrMagnitude < minSqrDistance)
 			{
 				minSqrDistance = (candidate.Item2 - candidate.Item1).sqrMagnitude;
@@ -58,7 +58,7 @@ namespace BlueUtils.Geometry
 			}
 
 			// Line 2 end
-			candidate = (ClosestPointOnLine(line1start, line1end, line2end), line2end);
+			candidate = (ClosestPointOnLineSegment(line1start, line1end, line2end), line2end);
 			if ((candidate.Item2 - candidate.Item1).sqrMagnitude < minSqrDistance)
 			{
 				minSqrDistance = (candidate.Item2 - candidate.Item1).sqrMagnitude;
@@ -66,7 +66,7 @@ namespace BlueUtils.Geometry
 			}
 
 			// Line 1 start
-			candidate = (ClosestPointOnLine(line2start, line2end, line1start), line1start);
+			candidate = (ClosestPointOnLineSegment(line2start, line2end, line1start), line1start);
 			if ((candidate.Item2 - candidate.Item1).sqrMagnitude < minSqrDistance)
 			{
 				minSqrDistance = (candidate.Item2 - candidate.Item1).sqrMagnitude;
@@ -74,7 +74,7 @@ namespace BlueUtils.Geometry
 			}
 
 			// Line 1 end
-			candidate = (ClosestPointOnLine(line2start, line2end, line1end), line1end);
+			candidate = (ClosestPointOnLineSegment(line2start, line2end, line1end), line1end);
 			if ((candidate.Item2 - candidate.Item1).sqrMagnitude < minSqrDistance)
 			{
 				// minSqrDistance = (candidate.Item2 - candidate.Item1).sqrMagnitude;
